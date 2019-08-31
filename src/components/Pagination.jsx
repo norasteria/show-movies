a8f00025ec5a4e68fe52fc20df0b0c62182af61c
Nora <nora@tebengan.id>
2019-08-31 07:27:48 +0700

    Fixing display pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Pagination from "react-paginate";
 
 export default ({ onPageChange, totalData }) => (
-  <div className="pagination" style={{ margin: "auto", display: "unset" }}>
+  <div className="pagination" style={{ margin: "auto", display: "block" }}>
     {totalData > 0 ? (
       <Pagination
         previousLabel="Prev"
