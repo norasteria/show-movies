3185df1c7afa44791aa7caa56410aa3beac08062
Nora <nora@tebengan.id>
2019-09-01 17:24:26 +0700

    Fixing loading display

--- src/pages/movieDetails/index.jsx
+++ src/pages/movieDetails/index.jsx
@@ -23,32 +23,26 @@ export default ({ match }) => {
         MOVIE DETAILS
       </h2>
       <hr />
-      {loading ? (
-        <div style={{ textAlign: "center" }}>
-          <Loader loading={true} sizeUnit={"px"} size={30} />
-        </div>
-      ) : (
-        <Row style={{ marginTop: 50 }}>
-          <Col style={{ textAlign: "center" }}>
-            <img
-              src={content.Poster}
-              alt="Poster"
-              style={{ borderRadius: 5 }}
-            />
-          </Col>
-          <Col>
-            <div>
-              <h2 style={{ display: "inline-block" }}>{content.Title}</h2> (
-              {content.Year})
-            </div>
-            <span>Genre : {content.Genre}</span>
-            <p>
-              Rating : {content.imdbRating} | Duration : {content.Runtime}
-            </p>
-            <p style={{ marginTop: 50 }}>{content.Plot}</p>
-          </Col>
-        </Row>
-      )}
+
+      <div style={{ textAlign: "center" }}>
+        <Loader loading={loading} sizeUnit={"px"} size={30} />
+      </div>
+      <Row style={{ marginTop: 50 }}>
+        <Col style={{ textAlign: "center" }}>
+          <img src={content.Poster} alt="Poster" style={{ borderRadius: 5 }} />
+        </Col>
+        <Col>
+          <div>
+            <h2 style={{ display: "inline-block" }}>{content.Title}</h2> (
+            {content.Year})
+          </div>
+          <span>Genre : {content.Genre}</span>
+          <p>
+            Rating : {content.imdbRating} | Duration : {content.Runtime}
+          </p>
+          <p style={{ marginTop: 50 }}>{content.Plot}</p>
+        </Col>
+      </Row>
     </Body>
   );
 };
